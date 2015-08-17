f5d604de80bb15adbe51da98232df0c1d178677d
dedenker <badcluster@hotmail.com>
2015-08-17 13:33:52 +0200

    Dockerfile updated with new ubuntu. Build updated with new openCV files

--- build_opencv.sh
+++ build_opencv.sh
@@ -1,7 +1,7 @@
-curl -L 'http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.7/opencv-2.4.7.tar.gz/' | tar xvzf -
-mkdir -p opencv-2.4.7/release
-cd opencv-2.4.7/release
+curl -L 'https://github.com/Itseez/opencv/archive/2.4.11.zip' | unzip -p -
+mkdir -p opencv-2.4.11/release
+cd opencv-2.4.11/release
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_PYTHON_SUPPORT=ON -D WITH_XINE=ON -D WITH_TBB=ON ..
 make && make install
 cd /
-rm -rf opencv-2.4.7
+rm -rf opencv-2.4.11
